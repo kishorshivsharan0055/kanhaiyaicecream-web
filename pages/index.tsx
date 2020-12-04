f857305cc82e9e44ac2441594f51477b1839c91f
kishor <kishorshivsharan8796@gmail.com>
2020-12-04 20:42:57 +0530

    :bug: fixed whatsapp style issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,17 @@ const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     extendedIcon: {
       marginRight: theme.spacing(1),
+      color: "#006400",
+    },
+    core: {
+      margin: 0,
+      top: "auto",
+      right: 20,
+      bottom: 20,
+      left: "auto",
+      position: "fixed",
+      color: "RGB(255, 255, 255)",
+      backgroundColor: "#2E8B57",
     },
   })
 );
@@ -33,16 +44,28 @@ export default function Home() {
       <SliderCorousal />
       <Products />
 
-      <ReactWhatsapp
-        number={PHONE_NO}
-        style={style}
-        children={
-          <div className="flex">
-            <WhatsAppIcon className={classes.extendedIcon} />
-            <div className="text-white font-semibold">Chat with Us</div>
-          </div>
-        }
-      />
+      <div
+        style={{
+          margin: 0,
+          top: "auto",
+          right: 20,
+          bottom: 20,
+          left: "auto",
+          position: "fixed",
+          color: "RGB(255, 255, 255)",
+          // backgroundColor: "#2E8B57",
+        }}
+      >
+        <ReactWhatsapp
+          number={PHONE_NO}
+          children={
+            <div className="flex">
+              <WhatsAppIcon className={classes.extendedIcon} />
+              <div className="text-gray-900 font-semibold">Chat with Us</div>
+            </div>
+          }
+        />
+      </div>
     </>
   );
 }
